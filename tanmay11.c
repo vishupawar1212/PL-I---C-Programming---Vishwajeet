517bc60eaea06e24fc153bc0dfe7d5a0de5aa699
vishupawar1212 <pawarvishwajeet35@gmail.com>
2026-09-19 15:22:20 +0530

    Add files via upload

---
+++ tanmay11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter your number:");
+    scanf("%d",&num);
+
+    if(num%2 == 0)
+    {
+    printf("the number is even.");
+    }
+    else 
+    {
+        printf("the number is odd.");
+   }
+   return 0;
+   
+}
