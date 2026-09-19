fe1a1e8528c2809171dc5246900b3ce1d948ce6d
vishupawar1212 <pawarvishwajeet35@gmail.com>
2026-09-19 15:22:36 +0530

    Add files via upload

---
+++ tanmay12.c
@@ -0,0 +1,21 @@
+//program(12)--Write a program to determine whether a given number is positive,negative,or zero.
+
+#include<stdio.h>
+int main()
+{
+int num;
+
+printf("Enter a number:");
+scanf("%d",&num);
+
+if(num>0)
+printf("The number is positive");
+
+else if(num<0)
+printf("The number is negative");
+
+else
+printf("The number is zero");
+
+return 0;
+}
